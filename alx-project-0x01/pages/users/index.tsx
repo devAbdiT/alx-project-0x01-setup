d49725ec60cf1a479119d8218448e57f803f4b01
Abdi begna <abditadesse89@gmail.com>
2025-11-16 23:35:49 +0300

    PostModals

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -15,6 +15,7 @@ const Users: React.FC<UserProps[]> = ({ posts }) => {
           </button>
         </div>
         <div className="grid grid-cols-3 gap-2">
+          {/* posts?.map */}
           {posts.map((user: UserProps) => (
             <UserCard
               key={user.id}
